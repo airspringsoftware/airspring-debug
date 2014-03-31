c6e054c07d8e86ce6ded7a2a7c96776975131eac
Jim Knochelmann <jim@airspringsoftware.com>
2014-03-31 10:31:54 -0400

    initial commit for airspring-debug
    Signed-off-by: Jim Knochelmann <jim@airspringsoftware.com>

---
+++ airDebug.js
@@ -0,0 +1,29 @@
+//Copyright©2012, 2013. Airspring Software LLC (“airSpring”). All Rights Reserved.
+
+/*
+	Usage:
+	var mydebug = require(logger.debugPath)('airspring:some:pattern');
+
+*/
+
+var debug = require('debug');
+
+module.exports = function( pattern ){
+
+	var _airDebug = debug(pattern);
+	var airDebug = null;
+
+	// In the case of tests logger may not exist, or may be a different
+	// logger object from the testing framework. Just use default behavior
+	// if we don't have an airspring logger.
+	if ( typeof logger !== 'undefined' && logger.isAirspring ) {
+		airDebug = function(){
+			logger.debug(_airDebug, arguments);
+		};
+	}
+	else{
+		airDebug = _airDebug;
+	}
+
+	return airDebug;
+};

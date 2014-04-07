fa768a5ba1350935b38e0bd7d84ff71ec9c272a1
Jim Knochelmann <jim@airspringsoftware.com>
2014-04-06 20:04:17 -0400

    finishes logger refactor

--- airDebug.js
+++ airDebug.js
@@ -2,21 +2,46 @@
 
 /*
 	Usage:
-	var mydebug = require(logger.debugPath)('airspring:some:pattern');
+		DEBUG=airspring:some:pattern node airspring.js
+			and/or
+		Enable debugging from client side server-stats panel
 
+		var mydebug = require('airspring-debug')('airspring:some:pattern');
+
+	Functionality provided by this wrapper:
+	- makes debug module configurable at run time
+	- sends logging through airLogger so it can be processed however we want
+	  * airLogger enables all by default, then we control what actually gets logged
+	    here in this wrapper
 */
 
 
-module.exports = function( pattern ){
+module.exports = function( pattern, logger ){
 
-	var debug = require('debug');
+	// allow a logger to be passed in for testing purposes
+	var airLogger  = (logger) ? logger : global.airLogger;
+
+	var debug = require('debug'),
+			_ = require('underscore');
+
 	var _airDebug = debug(pattern);
 	var airDebug = null;
 
-	// In the case of tests logger may not exist, or may be a different
-	// logger object from the testing framework. Just use default behavior
-	// if we don't have an airspring logger.
-	if ( typeof airLogger !== 'undefined' && airLogger.airspringDebug ) {
+	if ( typeof airLogger !== 'undefined') {
+		// we should never get the function disabled(){} from the debug module,
+		// because process.env.DEBUG is set in airLogger... register each function
+		// with the airLogger so it can be enabled/disabled at runtime.
+		var currentlyEnabled = false;
+
+		_.each(_.without(airLogger.get('DEBUG').split(','),""), function(requestedDebugPattern){
+			var re = new RegExp('^' + requestedDebugPattern.replace("*", ".*") + '$', 'i');
+			if (re.test(pattern)) currentlyEnabled = true;
+		});
+
+		_airDebug.currentlyEnabled = currentlyEnabled;
+		_airDebug.pattern = pattern;
+		airLogger.registerDebug(_airDebug);
+
 		airDebug = function(){
 			airLogger.debug(_airDebug, arguments);
 		};
@@ -27,3 +52,4 @@ module.exports = function( pattern ){
 
 	return airDebug;
 };
+
